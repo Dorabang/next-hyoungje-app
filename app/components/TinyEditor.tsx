d4257a242bfc7b005b340e53202d9743f20094e3
Dorabang <psa1195@naver.com>
2023-09-18 22:55:39 +0900

    feat: tinymce editor를 이용해 게시글 작성 컴포넌트 제작 (이미지 추가 기능 진행 중)

---
+++ app/components/TinyEditor.tsx
@@ -0,0 +1,64 @@
+'use client';
+import React, { useRef, useState } from 'react';
+import { storageService } from '@/firebase';
+import { Editor } from '@tinymce/tinymce-react';
+import { useRecoilState } from 'recoil';
+import { editorState } from '@/recoil/atoms';
+
+const TinyEditor = () => {
+  const editorRef = useRef(null);
+
+  const [value, setValue] = useRecoilState<string>(editorState);
+
+  const tinymcePlugin = [
+    'advlist',
+    'autolink',
+    'lists',
+    'link',
+    'image',
+    'charmap',
+    'preview',
+    'anchor',
+    'searchreplace',
+    'visualblocks',
+    'code',
+    'fullscreen',
+    'insertdatetime',
+    'media',
+    'table',
+    'code',
+    'help',
+    'wordcount',
+    'image',
+  ];
+
+  const tinymceToolbar =
+    'undo redo | blocks | ' +
+    'bold italic forecolor | alignleft aligncenter ' +
+    'alignright alignjustify | bullist numlist outdent indent | ' +
+    'removeformat | help' +
+    'image';
+
+  const imageFileType = 'jpg,svg,webp';
+
+  return (
+    <>
+      <Editor
+        apiKey={process.env.NEXT_PUBLIC_TINYMCE_API_KEY}
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        value={value}
+        onEditorChange={(newValue, editor) => setValue(newValue)}
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: tinymcePlugin,
+          toolbar: tinymceToolbar,
+          images_file_types: imageFileType,
+          language: 'ko_KR',
+        }}
+      />
+    </>
+  );
+};
+
+export default TinyEditor;
