cf69fedd954ed107cd8613c1a0c2bdd0f5e23335
Dorabang <psa1195@naver.com>
2023-09-18 22:58:52 +0900

    chore: 게시물 디테일 페이지 생성

---
+++ app/wild-market1/[id]/page.tsx
@@ -0,0 +1,11 @@
+interface WildMarketDetailPageProps {
+  params: { id: string };
+}
+
+const WildMarketDetailPage = ({
+  params: { id },
+}: WildMarketDetailPageProps) => {
+  return <div>WildMarketDetailPage</div>;
+};
+
+export default WildMarketDetailPage;
