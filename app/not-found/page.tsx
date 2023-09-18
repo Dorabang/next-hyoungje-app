24814e228c3f1fe086479222000109feb08a2662
Dorabang <psa1195@naver.com>
2023-09-18 22:53:16 +0900

    design: css 추가

--- app/not-found/page.tsx
+++ app/not-found/page.tsx
@@ -3,8 +3,12 @@ import { Button, Stack } from '@mui/material';
 const ErrorPage = () => {
   return (
     <div className='h-[600px] flex justify-center items-center'>
-      <Stack direction={'col'} spacing={8}>
-        <p>잘못된 접근입니다. 로그인 후, 이용해주세요.</p>
+      <Stack direction={'column'} spacing={4} alignItems={'center'}>
+        <p className='text-center'>
+          잘못된 접근입니다.
+          <br />
+          로그인 후, 이용해주세요.
+        </p>
         <div className='mx-auto'>
           <Button variant='contained' href='/' size='medium'>
             홈으로 돌아가기
