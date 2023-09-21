1d585239e3421d05847278e046a2f7476738146f
Dorabang <psa1195@naver.com>
2023-09-21 18:39:26 +0900

    feat: 새로운 글 작성 페이지 - 글 작성 후, 데이터를 firebase storage에 저장 - 이미지 데이터는 store에 저장 후, 저장된 url을 string[]에 담아서 storage에 저장

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -4,43 +4,70 @@ import ContainerBox from '@/components/ContainerBox';
 import TinyEditor from '@/components/TinyEditor';
 import useRedirect from '@/hooks/useRedirect';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { authState, editorState } from '@/recoil/atoms';
+import { authState, editorState, imageState } from '@/recoil/atoms';
 import { Button } from '@mui/material';
-import { getDownloadURL, ref, uploadString } from 'firebase/storage';
 import { dbService, storageService } from '@/firebase';
 import { addDoc, collection } from 'firebase/firestore';
 import uuid from 'react-uuid';
 import { useRouter } from 'next/navigation';
+import uploadImage from '@/utils/uploadImage';
+import Image from 'next/image';
+import { AiOutlineClose } from 'react-icons/ai';
+
+interface ImageObjProps {
+  id: string;
+  imageUrl: string;
+}
 
 const ModifyPostPage = ({ params: { id } }: { params: { id: string } }) => {
   useRedirect();
+
   const user = useRecoilValue(authState);
-  const [title, setTitle] = useState('');
-  const [image, setImage] = useState('');
-  const [value, setValue] = useRecoilState(editorState);
 
   const router = useRouter();
+
+  const statusList = [
+    { value: 'sale', desc: '판매 중' },
+    { value: 'sold-out', desc: '판매 완료' },
+    { value: 'reservation', desc: '예약 중' },
+  ];
+
+  const [title, setTitle] = useState('');
+  const [variant, setVariant] = useState('');
+  const [phone, setPhone] = useState('');
+  const [status, setStatus] = useState('sale');
+  const [price, setPrice] = useState('');
+  const [place, setPlace] = useState('');
+  const [date, setDate] = useState('');
+  const [height, setHeight] = useState('cm');
+  const [width, setWidth] = useState('cm');
+  const [amount, setAmount] = useState('');
+  const [image, setImage] = useRecoilState(imageState);
+  const [imageArr, setImageArr] = useState<ImageObjProps[] | null>(null);
+  const [value, setValue] = useRecoilState(editorState);
+
   const handleSubmit = async () => {
-    let attachmentUrl = '';
-    /* 
-    if (image !== '') {
-      const newPostImageRef = ref(
-        storageService,
-        `${id}/${user?.uid}_${uuid()}`
-      );
+    // const imageUrlArr = await uploadImage(id, image);
+    let newArr: string[] = [];
+    imageArr?.map(async (image) => {
+      const imageUrl = await uploadImage(id, image.imageUrl);
+      imageUrl && newArr.push(imageUrl);
+    });
 
-      await uploadString(newPostImageRef, image, 'data_url');
-
-      attachmentUrl = await getDownloadURL(
-        ref(storageService, newPostImageRef)
-      );
-    }
- */
     const newPostObj = {
       id: uuid(),
       title: title,
+      status: status,
+      variant: variant,
+      phone: phone,
+      place: place,
       contents: value,
-      attachmentUrl,
+      date: date,
+      price: price,
+      height: height,
+      width: width,
+      amount: amount,
+      image: newArr,
       like: [],
       comment: [],
       views: 0,
@@ -54,19 +81,210 @@ const ModifyPostPage = ({ params: { id } }: { params: { id: string } }) => {
     setTitle('');
     setImage('');
     setValue('');
+    setDate('');
+    setWidth('cm');
+    setHeight('cm');
+    setPlace('');
+    setPrice('');
+    setAmount('');
+    setStatus('');
     router.back();
   };
+
+  const onFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { files },
+    } = e;
+
+    if (files) {
+      const theFile = files[0];
+      const reader = new FileReader();
+
+      reader.onloadend = (finishedEvent) => {
+        const {
+          currentTarget: { result },
+        } = finishedEvent;
+
+        if (result) {
+          setImage(result);
+
+          const imageObj: ImageObjProps = { id: uuid(), imageUrl: result };
+
+          setImageArr((prev) =>
+            prev !== null ? [...prev, imageObj] : [imageObj]
+          );
+        }
+      };
+      reader.readAsDataURL(theFile);
+    }
+  };
+
+  const handleDeleteImage = (id: string) => {
+    const modifyImageArr = imageArr
+      ? imageArr?.filter((imageObj) => imageObj.id !== id)
+      : [];
+
+    setImageArr(modifyImageArr);
+  };
+
   return (
     <ContainerBox>
-      <div className='flex flex-col gap-4 justify-center'>
-        <form className='mb-3'>
-          <input
-            type='text'
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder='제목을 입력해주세요.'
-            className='w-full outline-none border-b border-[#dddddd] px-2 py-4'
-          />
+      <div className='flex flex-col gap-4 justify-center mx-4 sm:mx-0 '>
+        <form className='mb-3 flex flex-col justify-center [&_label]:w-[90px] [&_label]:border-r [&_label]:border-neutral-300'>
+          <div className='flex w-full border-b border-[#dddddd] p-2'>
+            <div className='pr-4'>
+              <select
+                id='status'
+                className='outline-none cursor-pointer'
+                onChange={(e) => setStatus(e.target.value)}
+                value={status}
+              >
+                {statusList.map((option) => (
+                  <option value={option.value} key={option.value}>
+                    {option.desc}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <input
+              type='text'
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder='* 제목을 입력해주세요.'
+              className='outline-none'
+              required
+            />
+          </div>
+
+          <div className='flex w-full border-b border-[#dddddd] p-2'>
+            <label htmlFor='variant'>* 종류</label>
+            <input
+              name='variant'
+              type='text'
+              value={variant}
+              onChange={(e) => setVariant(e.target.value)}
+              className='outline-none pl-3'
+              required
+            />
+          </div>
+
+          <div className='flex w-full border-b border-[#dddddd] p-2'>
+            <label htmlFor='phone'>* 연락처</label>
+            <input
+              name='phone'
+              type='text'
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              className='outline-none pl-3'
+              required
+            />
+          </div>
+
+          <div className='flex w-full border-b border-[#dddddd] p-2'>
+            <label htmlFor='place'>* 산지</label>
+            <input
+              name='place'
+              type='text'
+              value={place}
+              onChange={(e) => setPlace(e.target.value)}
+              className='outline-none pl-3'
+              required
+            />
+          </div>
+
+          <div className='flex w-full border-b border-[#dddddd] p-2'>
+            <label htmlFor='date'>* 산채일</label>
+            <input
+              name='date'
+              type='date'
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              className='outline-none pl-3'
+              required
+            />
+          </div>
+
+          <div className='flex w-full border-b border-[#dddddd] p-2'>
+            <label htmlFor='price'>* 가격</label>
+            <input
+              name='price'
+              type='text'
+              value={price.toLocaleString()}
+              onChange={(e) => setPrice(e.target.value)}
+              className='outline-none pl-3'
+              required
+            />
+          </div>
+
+          <div className='flex w-full border-b border-[#dddddd] p-2'>
+            <label htmlFor='height'>* 키</label>
+            <input
+              name='height'
+              type='text'
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+              className='outline-none pl-3'
+              required
+            />
+          </div>
+
+          <div className='flex w-full border-b border-[#dddddd] p-2'>
+            <label htmlFor='width'>* 폭</label>
+            <input
+              name='width'
+              type='text'
+              value={width}
+              onChange={(e) => setWidth(e.target.value)}
+              className='outline-none pl-3'
+              required
+            />
+          </div>
+
+          <div className='flex w-full border-b border-[#dddddd] p-2'>
+            <label htmlFor='amount'>촉수</label>
+            <input
+              name='amount'
+              type='text'
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              className='outline-none pl-3'
+            />
+          </div>
+
+          <div className='flex w-full border-b border-[#dddddd] p-2'>
+            <label htmlFor='phone'>파일 첨부</label>
+            <div className='flex flex-wrap pl-3'>
+              <input
+                id='addFile'
+                name='addFile'
+                type='file'
+                accept='image/*'
+                onChange={onFileChange}
+                className='outline-none w-full'
+              />
+              <ul className='py-4 flex gap-2'>
+                {imageArr &&
+                  imageArr.map((item) => (
+                    <li key={item.id}>
+                      <div className='w-[100px] h-[100px] relative flex gap-4 overflow-hidden'>
+                        <Image
+                          src={item.imageUrl}
+                          alt={`${item} 이미지`}
+                          fill
+                          className='object-cover'
+                        />
+                        <div
+                          className='absolute right-0 top-0 w-5 h-5 bg-black flex justify-center items-center cursor-pointer'
+                          onClick={() => handleDeleteImage(item.id)}
+                        >
+                          <AiOutlineClose className='text-white' />
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+              </ul>
+            </div>
+          </div>
         </form>
         <TinyEditor />
 
