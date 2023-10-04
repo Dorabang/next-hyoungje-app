83d4eb3deca37f71f8deee5828b9daa597b00337
Dorabang <psa1195@naver.com>
2023-10-04 22:46:39 +0900

    chore: 이미지 업로드 및 데이터 베이스에서 이미지 URL 받아오기 함수 path 수정

--- app/utils/uploadImage.ts
+++ app/utils/uploadImage.ts
@@ -1,13 +1,12 @@
 import { authService, storageService } from '@/firebase';
 import { getDownloadURL, ref, uploadString } from 'firebase/storage';
-import uuid from 'react-uuid';
 
 const uploadImage = async (path: string, url: string) => {
   const user = authService.currentUser;
 
   if (!user || !url) return;
 
-  const photoRef = ref(storageService, `/${path}/${user.uid}/${uuid()}.png`);
+  const photoRef = ref(storageService, `/${path}.png`);
   await uploadString(photoRef, url);
 
   const photoURL = await getDownloadURL(photoRef);
