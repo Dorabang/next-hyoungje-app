2056a5bf29ee2749a3431e30f959da9ec1d2e885
Dorabang <psa1195@naver.com>
2023-10-15 20:56:02 +0900

    chore: 기존 게시물 편집 기능구현 진행 중

---
+++ app/(home)/wild-market1/edit/[id]/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+import Edit from '@/components/Edit';
+import getPosts from '@/utils/getPosts';
+import { DocumentData } from 'firebase/firestore';
+import { usePathname } from 'next/navigation';
+import { useState, useEffect } from 'react';
+
+const DetailEditPage = ({ params: { id } }: { params: { id: string } }) => {
+  const pathname = usePathname();
+  const [posts, setPosts] = useState<DocumentData[]>([]);
+
+  const post = posts.find((item) => item.id === id);
+
+  useEffect(() => {
+    if (posts.length === 0) {
+      getPosts('wild-market1').then((response) => setPosts(response));
+    }
+  }, [posts.length]);
+
+  if (!post) return;
+  return <Edit post={post} />;
+};
+
+export default DetailEditPage;
