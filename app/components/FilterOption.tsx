9e6068b2e2da013e43b2f59826085dd09bce5434
Dorabang <psa1195@naver.com>
2023-10-21 00:45:27 +0900

    refactor: 필터 옵션 컴포넌트 분리

---
+++ app/components/FilterOption.tsx
@@ -0,0 +1,62 @@
+import { authState } from '@/recoil/atoms';
+import Link from 'next/link';
+import { HiOutlinePencilSquare } from 'react-icons/hi2';
+import { useRecoilValue } from 'recoil';
+
+interface FilterOptionProps {
+  selectedCategory: string;
+  handleUpdateFilter: (status: string) => void;
+  pathname: string;
+}
+
+const FilterOption = ({
+  selectedCategory,
+  handleUpdateFilter,
+  pathname,
+}: FilterOptionProps) => {
+  const user = useRecoilValue(authState);
+
+  const filters = [
+    { key: 'all', value: '전체' },
+    { key: 'sale', value: '판매 중' },
+    { key: 'sold-out', value: '판매 완료' },
+    { key: 'reservation', value: '예약 중' },
+  ];
+
+  return (
+    <ul
+      className='flex justify-end items-center gap-4 pt-10 pb-5
+      text-gray-500 text-sm'
+    >
+      {filters.map((filter) => (
+        <li
+          key={filter.key}
+          className={`cursor-pointer hover:text-gray-700 hover:font-medium hover:underline
+          ${selectedCategory === filter.key ? 'text-gray-700' : ''}
+          ${selectedCategory === filter.key ? 'font-medium' : ''}
+          ${selectedCategory === filter.key ? 'underline' : ''}
+          `}
+          onClick={() => handleUpdateFilter(filter.key)}
+        >
+          {filter.value}
+        </li>
+      ))}
+      {user && (
+        <>
+          <li className='cursor-default'>|</li>
+          <li>
+            <Link
+              href={`/edit/${pathname}`}
+              className='text-neutral-500 hover:text-neutral-800 flex items-center transition-colors'
+            >
+              <HiOutlinePencilSquare size={18} className='mr-1' />
+              글쓰기
+            </Link>
+          </li>
+        </>
+      )}
+    </ul>
+  );
+};
+
+export default FilterOption;
