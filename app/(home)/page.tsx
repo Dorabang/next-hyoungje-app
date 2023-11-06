bd46dd5d386bfecdeb5878048f37bb74364e19c7
Dorabang <psa1195@naver.com>
2023-11-06 20:03:30 +0900

    chore: 메인 슬라이드 1 추가

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,11 +3,12 @@ import Image from 'next/image';
 import mvDesktop from '@/assets/main_visual_pc.jpg';
 import mvTablet from '@/assets/main_visual_tablet.jpg';
 import mvMobile from '@/assets/main_visual_mobile.jpg';
+import MainSlide1 from '@/components/MainSlide1';
 
 export default function Home() {
   return (
     <main>
-      <div className='border-b border-neutral-300'>
+      <div className='border-b border-grayColor-200'>
         <Image
           src={mvDesktop}
           alt='산채인을 위한 산채품 전문 직거래 장터, 형제 난원'
@@ -27,7 +28,9 @@ export default function Home() {
           priority
         />
       </div>
-      <ContainerBox>Slider 1</ContainerBox>
+      <ContainerBox>
+        <MainSlide1 />
+      </ContainerBox>
       <ContainerBox>Slider 2</ContainerBox>
       <ContainerBox>Slider 3</ContainerBox>
     </main>
