0a8cd6eb79b00ef3c053cec1ad056d80f2496893
Dorabang <psa1195@naver.com>
2023-11-08 16:54:21 +0900

    chore: 불필요한 코드 수정

--- app/utils/getImageURL.ts
+++ app/utils/getImageURL.ts
@@ -4,11 +4,14 @@ import { getDownloadURL, ref } from 'firebase/storage';
 const GetImageURL = async (url: string, changeImg: (value: string) => void) => {
   const photoRef = ref(storageService, `${url}.jpg`);
 
+  let downloadURL;
   await getDownloadURL(photoRef)
     .then((url) => {
       return changeImg(url);
     })
     .catch((error) => console.log('DownloadURL error: ' + error));
+
+  return downloadURL;
 };
 
 export default GetImageURL;
