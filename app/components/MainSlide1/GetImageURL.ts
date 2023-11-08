e251d281a5ab876d9ad4493d0c44e6e1f9538b98
Dorabang <psa1195@naver.com>
2023-11-08 16:54:00 +0900

    feat: 메인 슬라이드용 downloadImageURL 함수 작성

---
+++ app/components/MainSlide1/GetImageURL.ts
@@ -0,0 +1,18 @@
+import { storageService } from '@/firebase';
+import { getDownloadURL, ref } from 'firebase/storage';
+
+const GetImageURL = async (
+  url: string,
+  id: string,
+  changeImg: (id: string, value: string) => void
+) => {
+  const photoRef = ref(storageService, `${url}.jpg`);
+
+  await getDownloadURL(photoRef)
+    .then((url) => {
+      changeImg(id, url);
+    })
+    .catch((error) => console.log('DownloadURL error: ' + error));
+};
+
+export default GetImageURL;
