9ee500874e3ed9a9ddc0eb7f14efd452993f2df3
Dorabang <psa1195@naver.com>
2023-11-17 14:05:11 +0900

    rename: 파일 위치 변경

--- app/components/PostsNotFound.tsx
+++
@@ -1,9 +0,0 @@
-const PostsNotFound = () => {
-  return (
-    <li className='py-3 h-52 flex justify-center items-center'>
-      <p className='text-neutral-500'>현재 게시물이 없습니다.</p>
-    </li>
-  );
-};
-
-export default PostsNotFound;
