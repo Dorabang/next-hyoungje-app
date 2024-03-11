be50c7ba10087a8120f2042e369d5ed8fb9d3dfe
Dorabang <psa1195@naver.com>
2024-03-11 17:35:38 +0900

    remove: 불필요한 파일 삭제

--- app/components/Board/Bodys.tsx
+++
@@ -1,8 +0,0 @@
-import PostsLoading from '../Posts/PostsLoading';
-import PostsNotFound from '../Posts/PostsNotFound';
-import { doc, updateDoc } from 'firebase/firestore';
-import { dbService } from '@/firebase';
-import { useContext } from 'react';
-import { PostContext } from '.';
-import { deletePost } from '@/apis/posts';
-import PostList from './PostList';
