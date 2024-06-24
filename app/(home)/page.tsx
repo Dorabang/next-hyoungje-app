7c7dee20f7768d499980a3f6c293600036a6fac7
Dorabang <psa1195@naver.com>
2024-06-24 21:06:01 +0900

    design: 전체 레이아웃 수정

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <main>
       <MainVisual />
 
-      <ContainerBox className='flex flex-col gap-20 md:gap-[150px] py-20'>
+      <ContainerBox className='flex flex-col gap-20 xl:gap-[150px] py-5 xl:py-20'>
         <MainSlide
           path='wild-market1'
           title='산채품 장터'
@@ -26,10 +26,8 @@ export default function Home() {
         />
       </ContainerBox>
       <div className='border-t border-grayColor-200 mt-20 lg:mt-36 pt-10'>
-        <ContainerBox>
-          <div
-            className={`w-full text-center xl:w-[25%] mx-3 xl:mx-0 xl:text-left pb-5`}
-          >
+        <ContainerBox className='flex flex-col gap-20 py-5 xl:py-20'>
+          <div className='w-full text-center'>
             <p className='text-grayColor-400 text-lg'>산채품 정보 공유</p>
             <h3>
               <Link
@@ -40,7 +38,22 @@ export default function Home() {
               </Link>
             </h3>
           </div>
-          <MainCommunity />
+          <div className='flex flex-col gap-20 xl:gap-[150px]'>
+            <MainSlide
+              path='wild-catch'
+              title='산채기'
+              subTitle='산채품 기록 커뮤니티'
+              variant='center'
+              community
+            />
+            <MainSlide
+              path='boast'
+              title='난자랑'
+              subTitle='산채품·집채품 자랑 커뮤니티'
+              variant='left'
+              community
+            />
+          </div>
         </ContainerBox>
       </div>
     </main>
