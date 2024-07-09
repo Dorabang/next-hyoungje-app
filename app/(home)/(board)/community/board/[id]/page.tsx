703e11f69f1f73d446bed6d07541b55140307eb1
Dorabang <psa1195@naver.com>
2024-07-09 20:47:29 +0900

    refactor: suspense 추가
    - npm i next@latest

--- app/(home)/(board)/community/board/[id]/page.tsx
+++ app/(home)/(board)/community/board/[id]/page.tsx
@@ -1,11 +1,17 @@
+import { Suspense } from 'react';
 import CommDetailPage from '@/components/Community/CommDetail';
+import Loading from '@/components/Loading';
 
 interface NoticeDetailPageProps {
   params: { id: string };
 }
 
 const NoticeDetailPage = ({ params: { id } }: NoticeDetailPageProps) => {
-  return <CommDetailPage postId={id} />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <CommDetailPage postId={id} />
+    </Suspense>
+  );
 };
 
 export default NoticeDetailPage;
--- app/(home)/(board)/community/wild-catch/edit/[id]/page.tsx
+++ app/(home)/(board)/community/wild-catch/edit/[id]/page.tsx
@@ -5,12 +5,21 @@ import { usePathname } from 'next/navigation';
 import CommEdit from '@/components/Community/CommEdit';
 import Loading from '@/components/Loading';
 import { useGetPost } from '@/hooks/queries/usePosts';
+import ContainerBox from '@/components/ContainerBox';
 
 const ModifyPostPage = ({ params: { id } }: { params: { id: string } }) => {
   const pathname = usePathname().split('/')[2];
-  const { data } = useGetPost(pathname, id);
+  const { data, isLoading } = useGetPost(pathname, id);
 
-  if (!data) return;
+  if (isLoading) return <Loading />;
+
+  if (!data)
+    return (
+      <ContainerBox className='py-20'>
+        삭제된 게시물이거나 찾을 수 없는 게시물입니다.
+      </ContainerBox>
+    );
+
   return (
     <Suspense fallback={<Loading />}>
       <CommEdit post={data} pathname={pathname} />
