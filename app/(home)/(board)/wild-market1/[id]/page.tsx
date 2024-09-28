2fd2aac2dd80161237e57860e66813145e9529d1
Dorabang <psa1195@naver.com>
2024-09-28 20:07:55 +0900

    refactor: seo 최적화를 위해 동적 metadata 설정할 수 있도록 수정

--- app/(home)/(board)/wild-market1/[id]/page.tsx
+++ app/(home)/(board)/wild-market1/[id]/page.tsx
@@ -1,38 +1,74 @@
-'use client';
-import { Suspense } from 'react';
+import { Fragment } from 'react';
 
 import ContainerBox from '@/components/common/ContainerBox';
-import Loading from '@/components/common/Loading';
 import PostDetail from '@/components/Posts/PostDetail';
-import { usePost } from '@/hooks/queries/usePosts';
+import { getPost } from '@/apis/posts';
 
 interface WildMarketDetailPageProps {
   params: { id: number };
 }
 
-const WildMarketDetailPage = ({
+export const generateMetadata = async ({
+  params: { id },
+}: {
+  params: { id: number };
+}) => {
+  const data = await getPost(id);
+
+  return {
+    title: `옥동 || ${data.post.title}`,
+    description: '한국춘란 산채품 전문 직거래장터 || 산채품장터1',
+    metadataBase: new URL('https://www.hyoungje.kr'),
+    openGraph: {
+      title: `옥동 || ${data.post.title}`,
+      description: data.post.contents,
+      images: [{ url: data.post.image }],
+    },
+    robots: {
+      index: false,
+      follow: true,
+      nocache: true,
+      googleBot: {
+        index: true,
+        follow: false,
+        noimageindex: true,
+      },
+    },
+    keywords: [
+      '옥동',
+      '형제난원',
+      '산채품',
+      '한국춘란',
+      '난',
+      '난초',
+      '산채품 직거래',
+      '난초 직거래',
+      '산채',
+      '집채',
+      '춘란',
+      '약초',
+      data.post.variant,
+      data.post.place,
+      data.post.title,
+    ],
+  };
+};
+
+const WildMarketDetailPage = async ({
   params: { id },
 }: WildMarketDetailPageProps) => {
-  const { data, isLoading } = usePost(id);
-
-  if (isLoading)
-    return (
-      <ContainerBox className='py-20'>
-        <Loading />
-      </ContainerBox>
-    );
-
-  if (!data || !data?.post)
-    return (
-      <ContainerBox className='py-20'>
-        삭제된 게시물이거나 찾을 수 없는 게시물입니다.
-      </ContainerBox>
-    );
+  const data = await getPost(id);
 
   return (
-    <Suspense fallback={<Loading />}>
-      <PostDetail postId={id} data={data} />
-    </Suspense>
+    <Fragment>
+      {!data || !data?.post ? (
+        <ContainerBox className='py-20'>
+          삭제된 게시물이거나 찾을 수 없는 게시물입니다.
+        </ContainerBox>
+      ) : (
+        <PostDetail postId={id} data={data} />
+      )}
+    </Fragment>
   );
 };
 
