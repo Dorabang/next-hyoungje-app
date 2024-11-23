330f39b51e8a67b08455f72952ddbdf51b327485
Dorabang <psa1195@naver.com>
2024-11-23 22:00:41 +0900

    refactor: QueryClientWrapper > Provider로 파일명 변경 및 ssr에 맞게 코드 수정

---
+++ app/components/common/Wrapper/Provider.tsx
@@ -0,0 +1,47 @@
+'use client';
+import React, { ReactNode } from 'react';
+import {
+  QueryClientProvider,
+  QueryClient,
+  isServer,
+} from '@tanstack/react-query';
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
+
+const makeQueryClient = () => {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000, // 1 minutes
+        refetchOnMount: true,
+        refetchOnWindowFocus: true,
+        retry: 3,
+        retryDelay: (attemptNumber) => attemptNumber * 1000,
+        gcTime: 5 * 60 * 1000, // 5 minutes
+        refetchInterval: 10 + 60 * 1000, // 10 minutes
+      },
+    },
+  });
+};
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+const getQueryClient = () => {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+};
+
+const Provider = ({ children }: { children: ReactNode }) => {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+    </QueryClientProvider>
+  );
+};
+
+export default Provider;
